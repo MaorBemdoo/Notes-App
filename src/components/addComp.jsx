685b9ae0f133115f23fda778b4e8e9c79cb792ae
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-13 19:43:54 +0000

    Added a addComp.css file

---
+++ src/components/addComp.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import '../styles/addComp.css'
+
+const AddComp = ({addIcon}) => {
+    return (
+        <div id="addDiv">
+            <img src={addIcon} />
+        </div>
+    )
+}
+
+export default AddComp
