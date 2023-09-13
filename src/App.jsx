26f391d5e417a71d7d1f263205d8411255f207f4
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-13 15:44:23 +0000

    Mainly started styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import './App.css'
+import addIcon from './assets/add-icon.svg'
 
 function App() {
 
   return (
     <>
-      
+      <div id="addDiv">
+        <img src={addIcon}/>
+      </div>
     </>
   )
 }
