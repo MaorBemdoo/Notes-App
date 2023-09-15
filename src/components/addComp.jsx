6e41e7875b996297848b251d23cde50ae603a33f
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-15 15:13:04 +0000

    Implemented back-icon.svg

--- src/components/addComp.jsx
+++ src/components/addComp.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
 import '../styles/addComp.css'
 import submitIcon from '../assets/submit-icon.svg'
+import backIcon from '../assets/back-icon.svg'
 
 const AddComp = ({addIcon, onClickEvent, clicked, isClicked, onSubmitEvent, onChangeEvent, content}) => {
     return (
         clicked ? (
             <form method="post" id='addForm' onSubmit={onSubmitEvent}>
                 <textarea onChange={onChangeEvent} value={content}></textarea>
-                <button><img src={} alt='back'/></button>
+                <button><img src={backIcon} alt='back'/></button>
                 <button role='submit'><img src={submitIcon} alt='submit'/></button>
             </form>
         ) : (
