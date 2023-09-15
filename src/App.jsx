eab981ac49daa31029c1be6fa9974495bb6cc982
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-15 13:36:24 +0000

    Included useState of content

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import NoteDivCompilation from './components/notesComp';
 import { useState } from 'react';
 
 function App() {
+  const [content, setContent] = useState('')
   const [notes, setNotes] = useState([])
   const [clicked, isClicked] = useState(false)
+
+  const onChangeEvent = (e) => {
+    setContent(e.target.value)
+  }
 
   const onSubmitEvent = (e) => {
     e.preventDefault()
@@ -15,7 +20,7 @@ function App() {
 
   return (
     <>
-      <AddComp addIcon={addIcon} onClickEvent={() => isClicked(!clicked)} clicked={clicked} onSubmitEvent={onSubmitEvent}/>
+      <AddComp addIcon={addIcon} onClickEvent={() => isClicked(!clicked)} clicked={clicked} onSubmitEvent={onSubmitEvent} onChangeEvent={onChangeEvent} content={content}/>
       <NoteDivCompilation notes={notes}/>
     </>
   )
--- src/components/addComp.jsx
+++ src/components/addComp.jsx
@@ -2,11 +2,11 @@
 import '../styles/addComp.css'
 import submitIcon from '../assets/submit-icon.svg'
 
-const AddComp = ({addIcon, onClickEvent, clicked, onSubmitEvent}) => {
+const AddComp = ({addIcon, onClickEvent, clicked, onSubmitEvent, onChangeEvent, content}) => {
     return (
         clicked ? (
             <form method="post" id='addForm' onSubmit={onSubmitEvent}>
-                <textarea></textarea>
+                <textarea onChange={onChangeEvent} value={content}></textarea>
                 <button role='submit'><img src={submitIcon} alt='submit'/></button>
             </form>
         ) : (
