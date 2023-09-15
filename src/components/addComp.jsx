abb2a13645008b2743463874cf566be2b4011211
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-15 12:28:01 +0000

    Created jsx of #addForm

--- src/components/addComp.jsx
+++ src/components/addComp.jsx
@@ -4,9 +4,9 @@ import '../styles/addComp.css'
 const AddComp = ({addIcon, onClickEvent, clicked}) => {
     return (
         clicked ? (
-            <div>
-                Hey
-            </div>
+            <form method="post" id='addForm'>
+                <input type='text'/>            
+            </form>
         ) : (
             <div id="addDiv" onClick={onClickEvent}>
                 <img src={addIcon} />
