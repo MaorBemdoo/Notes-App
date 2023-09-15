1822515b1f9969a63d9cdc33850ac9973081720b
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-15 17:06:41 +0000

    Implemented send icon svg

--- src/components/noteComp.jsx
+++ src/components/noteComp.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 import '../styles/noteComp.css'
 import menuIcon from '../assets/menu-icon.svg'
+import sendIcon from '../assets/send-icon.svg'
 
 const NoteDiv = ({content, bgColor}) => {
     return (
         <div className='note'>
             <textarea style={{backgroundColor: bgColor}} disabled>{content}</textarea>
+            <div>
+                <img src={sendIcon} alt='send'/>
+            </div>
+
             <div>
                 <img src={menuIcon} alt='menu icon'/>
             </div>
