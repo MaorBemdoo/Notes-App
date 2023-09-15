683934819180afe95cbe047ccdd9e6281f38aaa6
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-15 13:10:41 +0000

    Added a submit svg and implemented it

--- src/components/addComp.jsx
+++ src/components/addComp.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 import '../styles/addComp.css'
+import submitIcon from '../assets/submit-icon.svg'
 
 const AddComp = ({addIcon, onClickEvent, clicked}) => {
     return (
         clicked ? (
             <form method="post" id='addForm'>
                 <textarea></textarea>
+                <button role='submit'><img src={submitIcon} alt='submit'/></button>
             </form>
         ) : (
             <div id="addDiv" onClick={onClickEvent}>
