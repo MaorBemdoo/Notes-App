52122a30b6c51487fe32aea4965711070750ae8b
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-28 17:41:10 +0100

    Removed editNote from prop drilling

--- src/components/notesComp.jsx
+++ src/components/notesComp.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import NoteDiv from './noteComp'
 
-const NoteDivCompilation = ({notes, deleteNote, editNote, edited}) => {
+const NoteDivCompilation = ({notes, deleteNote, edited}) => {
     return (
         <>
             {notes.map(({content, id, bgColor}) => {
                 // console.log(content);
-                return <NoteDiv content={content} key={id} bgColor={bgColor} id={id} deleteNote={deleteNote} editNote={editNote} edited={edited}/>
+                return <NoteDiv content={content} key={id} bgColor={bgColor} id={id} deleteNote={deleteNote} edited={edited}/>
             })}
         </>
     )
