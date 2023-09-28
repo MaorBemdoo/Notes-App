672d9875f612c32a683e22b0b4cc815fe013f002
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-28 23:44:08 +0100

    Added few fixes

--- src/components/notesComp.jsx
+++ src/components/notesComp.jsx
@@ -2,19 +2,14 @@
 import NoteDiv from './noteComp'
 
 const NoteDivCompilation = ({notes, deleteNote, edited, propDrilledSubmitHandler}) => {
-    if(localStorage.getItem("noteStore") !== null){
-        return (
-            <>
-                {JSON.parse(localStorage.getItem("noteStore")).map(({content, id, bgColor}) => {
-                    // console.log(content);
-                    return <NoteDiv content={content} key={id} bgColor={bgColor} id={id} deleteNote={deleteNote} edited={edited} propDrilledSubmitHandler={propDrilledSubmitHandler}/>
-                })}
-            </>
-        )
-    }
-    else{
-        localStorage.setItem("noteStore", "[]")
-    }
+    return (
+        <>
+            {notes.map(({content, id, bgColor}) => {
+                // console.log(content);
+                return <NoteDiv content={content} key={id} bgColor={bgColor} id={id} deleteNote={deleteNote} edited={edited} propDrilledSubmitHandler={propDrilledSubmitHandler}/>
+            })}
+        </>
+    )
 }
 
 export default NoteDivCompilation
