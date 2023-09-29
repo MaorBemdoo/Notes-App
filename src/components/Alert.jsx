48c1bb87ad6850f45c0f840062c8dc54121b76ae
Bemdoo Maor <bemdoo.maor1@gmail.com>
2023-09-29 12:33:22 +0100

    Worked on show toggle

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../styles/alert.css'
 import { BsInfoCircle } from 'react-icons/bs'
+import { useState } from 'react'
 
 function Alert() {
+    const [show, setShow] = useState(false)
+
     return (
-        <div id='formAlert'>
-            <BsInfoCircle id='infoIcon'/>
+        <div id='formAlert' className={show && "show"}>
+            <BsInfoCircle id='infoIcon' onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, alias.</p>
         </div>
     )
